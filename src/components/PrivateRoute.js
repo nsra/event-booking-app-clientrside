b37ed268a4737795c717b7513bc73d2a504a5e5f
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2021-10-19 19:39:01 +0300

    befor bootstrap

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,8 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
-import AuthContext from '../context/auth-context'
 
 export default function PrivateRoute({ path, component }) {
-    const value = useContext(AuthContext)
-    if (!value.token) return <Redirect to="/login" />
+    if (!localStorage.getItem("token")) return <Redirect to="/login" />
     else
     return <Route path={path} component={component} />;
 }
